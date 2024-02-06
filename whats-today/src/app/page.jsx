5f06bbbe68efb09ca44cdce8447c88b372072429
Arvi Naufal Agustian <arvinaufalagustian@gmail.com>
2024-02-06 09:49:44 +0700

    Fix: Wrap the useSearchParams

--- whats-today/src/app/page.jsx
+++ whats-today/src/app/page.jsx
@@ -2,6 +2,7 @@
 import Navbar from "@/components/organisms/Navbar";
 import Page from "@/components/organisms/PageHome";
 import Posts from "@/components/templates/Posts";
+import { Suspense } from "react";
 
 export default async function Home() {
 
@@ -9,7 +10,9 @@ export default async function Home() {
     <div className="w-screen min-h-screen items-center bg-orange-200">
       <div className="flex flex-col w-full items-center md:pb-10">
         <Navbar />
+        <Suspense fallback={<div>Loading ...</div>} >
           <Posts />
+        </Suspense>
       </div>
       <div className="flex w-full justify-center items-center py-4">
         <Page />
